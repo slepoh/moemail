f017aacde0d21f30ce02f5d2dcd9e0851c584f6e
尚谕 <22502073+slepoh@users.noreply.github.com>
2026-02-05 14:43:14 +0800

    Change GitHub link to cojs.cn in float-menu

--- app/components/float-menu.tsx
+++ app/components/float-menu.tsx
@@ -29,7 +29,7 @@ export function FloatMenu() {
               variant="outline"
               size="icon"
               className="bg-white dark:bg-background rounded-full shadow-lg group relative border-primary/20"
-              onClick={() => window.open("https://github.com/beilunyang/moemail", "_blank")}
+              onClick={() => window.open("http://cojs.cn", "_blank")}
             >
               <Github 
                 className="w-4 h-4 transition-all duration-300 text-primary group-hover:scale-110"
