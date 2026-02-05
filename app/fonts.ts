f65ea8585574a11685637a263887cb60a6f44123
尚谕 <22502073+slepoh@users.noreply.github.com>
2026-02-05 14:55:35 +0800

    Update fonts.ts

--- app/fonts.ts
+++ app/fonts.ts
@@ -1,7 +1,8 @@
 import localFont from 'next/font/local'
 
 export const zpix = localFont({
-  src: '../public/fonts/zpix.ttf',
+  //src: '../public/fonts/zpix.ttf',
+  src: '../public/fonts/harmonyos.ttf',
   variable: '--font-zpix',
   display: 'swap',
 }) 
